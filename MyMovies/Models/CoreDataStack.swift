f373dd89ffe819c28a5fe760f7afb47d3a351eb9
Enayatullah Naseri <ena.nas@icloud.com>
2019-12-14 09:44:58 -0800

    Started project created data model

---
+++ MyMovies/Models/CoreDataStack.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Enayatullah Naseri on 12/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    
+}
---
+++ MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,14 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Enayatullah Naseri on 12/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    
+}
